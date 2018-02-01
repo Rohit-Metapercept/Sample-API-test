4b8adce8a505cbf7ff2704c4b5f0a1d229c8007f
Rohit Ghosh <rohit@metapercept.com>
2018-02-01 13:18:00 +0530

    error test
    v3

---
+++ tokentest/env/auth/auth/urls.py
@@ -0,0 +1,29 @@
+"""auth URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from rest_framework.authtoken import views
+
+from django.contrib.auth import views as auth_views
+from tok import views as tok_views
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+	url(r'^api-token-auth/', views.obtain_auth_token),
+	url(r'^signup/$', views.signup, name='signup'),
+	url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',views.activate, name='activate'),
+	
+	]
